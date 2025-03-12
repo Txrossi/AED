87449502dea7498dbe3d3acc4a5792b63c9de607
TulioXRossi <tulioxr@hotmail.com>
2025-03-12 12:57:10 -0300

    Add deps file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/tulio/Repo/AED
+# Build directory: C:/Users/tulio/Repo/AED/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/tulio/Repo/AED/tests
+# Build directory: C:/Users/tulio/Repo/AED/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(calculator_test_name "test_calculadora")
+set_tests_properties(calculator_test_name PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/tulio/Repo/AED/tests/CMakeLists.txt;18;add_test;C:/Users/tulio/Repo/AED/tests/CMakeLists.txt;0;")
+add_test(linked_list_test_name "linked_test")
+set_tests_properties(linked_list_test_name PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/tulio/Repo/AED/tests/CMakeLists.txt;23;add_test;C:/Users/tulio/Repo/AED/tests/CMakeLists.txt;0;")
